025e25fcc983355b412e3732d745128daf9d9a54
paviem <128893079+paviem@users.noreply.github.com>
2025-09-06 19:31:11 +0800

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -151,8 +151,12 @@ func (c *Config) Validate() error {
 func (c *Config) validateListIdentifiers(lids []string) error {
 	re := regexp.MustCompile(`^ls\d{9,10}$`)
 	for _, id := range lids {
+		id = strings.TrimSpace(id)
 		if !re.MatchString(id) {
-			return fmt.Errorf("valid list id starts with ls and is followed by 9 or 10 digits, but got %s", id)
+			return fmt.Errorf(
+				"valid list id starts with ls and is followed by 9 or 10 digits, but got %s",
+				id,
+			)
 		}
 	}
 	return nil
